ccc94a73fc6acec4347bff03c3f69fcd960a692f
Daniel <5806484+hitboxturtle@users.noreply.github.com>
2019-08-09 11:23:17 +0100

    Feature/mer 501 (#2)
    * stuff
    
    * update to use param input for id

--- ci.ps1
+++ ci.ps1
@@ -1,6 +1,9 @@
-param( $testSuiteId = $env:AUTOMATOR_TEST_SUITE_ID )
+param( [string]$testSuiteId = $env:AUTOMATOR_TEST_SUITE_ID )
+
 $token = $env:AUTOMATOR_TOKEN
+
 Write-Output $testSuiteId
+
 $body = @{
     "authToken"   = $token
     "testSuiteId" = $testSuiteId
@@ -9,7 +12,7 @@ $body = @{
 [int]$totalRunTime = 0
 [int]$maxRunTime = $env:AUTOMATOR_TIMEOUT_SEC
 
-$meridianResults = $null;
+$testResults = $null;
 
 function Get-Results {
     Param([string]$Uri, $BuildId)
@@ -23,7 +26,7 @@ function Get-Results {
     }
     catch [System.Net.WebException] {
         Write-Verbose "An exception was caught: $($_.Exception.Message)"
-        return $null;
+        exit 1;
     }
     Write-Output $resultResponse
 }
@@ -31,7 +34,7 @@ function Get-Results {
 function Write-Results {
     Param($resultsData)
     Write-Output $resultsData
-    if ($meridianResults.passed -eq $true) {
+    if ($testResults.passed -eq $true) {
         #have tests passed 
         Write-Output "DeepCrawl Tests Passed"
         exit 0
@@ -45,10 +48,10 @@ function Write-Results {
 
 function Start-Poll {
     Param($BuildId)
-    $meridianResults = Get-Results -Uri $env:AUTOMATOR_POLL_URL -BuildId $BuildId
-    if ( [bool]($meridianResults.PSobject.Properties.name -match "passed")) {
+    $testResults = Get-Results -Uri $env:AUTOMATOR_POLL_URL -BuildId $BuildId
+    if ( [bool]($testResults.PSobject.Properties.name -match "passed")) {
         #do results contain the passed prop
-        Write-Results($meridianResults)
+        Write-Results($testResults)
     }
     else {
         Write-Output "Waiting for DeepCrawl Test Results ..."
@@ -63,7 +66,7 @@ function Start-Build {
     $triggerResponse = Invoke-RestMethod -Uri $env:AUTOMATOR_START_URL -Method "Post" -Body ($body | ConvertTo-Json) -ContentType "application/json";
     Write-Output $triggerResponse;
 
-    while ( ($null -eq $meridianResults) -and ($totalRunTime -lt $maxRunTime) ) {
+    while ( ($null -eq $testResults) -and ($totalRunTime -lt $maxRunTime) ) {
         #poll server
         Start-Poll -BuildId $triggerResponse.buildId
     }
