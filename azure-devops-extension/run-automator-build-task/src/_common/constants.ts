968a2e0f09ba94d78ccabf8a6ef3caacfefa8e22
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2020-10-09 15:11:00 +0100

    set max polling to 50 minutes

--- azure-devops-extension/run-automator-build-task/src/_common/constants.ts
+++ azure-devops-extension/run-automator-build-task/src/_common/constants.ts
@@ -4,7 +4,7 @@ export const TOOLS_API_START_BUILD_URL = "/start";
 export const URL_FOR_AUTH_TOKEN = "https://graph.staging.deepcrawl.com";
 export const POLLING_INTERVAL = 30000; // 30 seconds
 export const HTTP_STATUS_CODE_OK = 200;
-export const MAX_TIME_SPEND_ON_POLLING = 300000; // 3000 seconds
+export const MAX_TIME_SPEND_ON_POLLING = 3000000; // 3000 seconds
 
 export const inputParameters = {
   userKeyId: "userKeyId",
