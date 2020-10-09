1304a1d8c644c7e4ed11b3684004b8846b2c5a0e
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2020-10-09 13:45:55 +0100

    Add release process

---
+++ azure-pipelines-extension/release.sh
@@ -0,0 +1,19 @@
+rm -rf .tmp
+mkdir .tmp
+cp vss-extension.json .tmp/.
+cp package.json .tmp/.
+cp yarn.lock .tmp/.
+cp -R images .tmp/.
+cd run-automator-build-task && yarn && yarn build && cd ../
+cd .tmp
+yarn
+mkdir run-automator-build-task
+cp -R ../run-automator-build-task/dist run-automator-build-task/.
+cp ../run-automator-build-task/task.json run-automator-build-task/.
+cp ../run-automator-build-task/package.json run-automator-build-task/.
+cp ../run-automator-build-task/yarn.lock run-automator-build-task/.
+cd run-automator-build-task
+yarn install --production
+cd ../
+yarn tfx extension create --manifest-globs vss-extension.json --output-path ../releases/.
+cd ../ && rm -rf .tmp
