b2c0a2fab4e79ec1c2beac533017338d37f0bec9
Andrei Stefan Bitca <andrei.bitca@deepcrawl.com>
2020-10-12 17:17:17 +0100

    more tests

--- azure-devops-extension/run-automator-build-task/src/sdk/graph-api/client.test.ts
+++ azure-devops-extension/run-automator-build-task/src/sdk/graph-api/client.test.ts
@@ -1,61 +1,58 @@
-import nock from "nock";
+import { GraphQLError } from "graphql";
+import * as nock from "nock";
 
 import { GraphAPIClient } from "./client";
 
 const url = "http://test.com";
 const graphAPIClient = new GraphAPIClient({ url });
 
-enum ContextType {
-  APIThrowsError,
-  APIReturnsToken
-}
-
-interface IArgs {
-  userKeyId: string,
-  userKeySecret: string,
-}
-
-interface IContextOptions {
-  type: ContextType
-  args: IArgs;
-}
-
-function getContext({ type, args }: IContextOptions): Promise<string> {
-  const response = type === ContextType.APIThrowsError 
-    ? {
-      errors: [new Error("API Error")]
-    }
-    : {
-      data: {
-        createSessionUsingUserKey: {
-          token: Object.values(args).join(","),
-        },
-      },
-    };
-  nock(url).post("/").reply(200, response);
-  return graphAPIClient.getAuthToken(args.userKeyId, args.userKeySecret);
-};
-
 describe("GraphAPIClient", () => {
   describe("#getAuthToken", () => {
     it("should throw error if API throws error", async () => {
-      const response = await getContext({
-        type: ContextType.APIThrowsError,
-        args: {
-          userKeyId: "user-key-id",
-          userKeySecret: "user-key-secret",
-        },
+      const userKeyId = "user-key-id";
+      const userKeySecret = "user-key-secret";
+      const error = new GraphQLError("API Error");
+      nock(url).post("/").reply(200, {
+        data: null,
+        errors: [error]
       });
-      await expect(response).rejects.toEqual(new Error("API Error"));
+      await expect(graphAPIClient.getAuthToken(userKeyId, userKeySecret)).rejects.toEqual(error);
     });
+
     it("should return token", async () => {
       const userKeyId = "user-key-id";
       const userKeySecret = "user-key-secret";
-      const response = await getContext({
-        type: ContextType.APIReturnsToken,
-        args: { userKeyId, userKeySecret },
+      nock(url).post("/").reply(200, {
+        data: {
+          createSessionUsingUserKey: {
+            token: `${userKeyId},${userKeySecret}`,
+          },
+        },
       });
-      await expect(response).toEqual(`${userKeyId},${userKeySecret}`);
+      expect(await graphAPIClient.getAuthToken(userKeyId, userKeySecret)).toEqual(`${userKeyId},${userKeySecret}`);
+    });
+  });
+  describe("#deleteAuthToken", () => {
+    it("should throw error if API throws error", async () => {
+      const token = "token";
+      const error = new GraphQLError("API Error");
+      nock(url).post("/").reply(200, {
+        data: null,
+        errors: [error]
+      });
+      await expect(graphAPIClient.deleteAuthToken(token)).rejects.toEqual(error);
+    });
+
+    it("should return token", async () => {
+      const token = "token";
+      nock(url).post("/").reply(200, {
+        data: {
+          deleteSession: {
+            token,
+          },
+        },
+      });
+      expect(await graphAPIClient.deleteAuthToken(token)).toEqual(token);
     });
   });
 });
