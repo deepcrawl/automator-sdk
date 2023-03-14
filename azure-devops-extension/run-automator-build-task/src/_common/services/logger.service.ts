e57f811786bf7df3ad0e88e61aca982f2df476f7
Francesco Iesu <francesco.iesu@deepcrawl.com>
2023-03-14 17:30:30 +0000

    chore: replace logger

--- azure-devops-extension/run-automator-build-task/src/_common/services/logger.service.ts
+++ azure-devops-extension/run-automator-build-task/src/_common/services/logger.service.ts
@@ -1,8 +1,17 @@
-import pino from "pino";
+import {createLogger, format, transports} from "winston";
 
-export const loggerService = pino({
-  level: "info",
-  transport: {
-    target: "pino-pretty",
-  }
+const {colorize, combine, timestamp, printf} = format;
+const upperLevel = format(info => {
+  info.level = info.level.toUpperCase();
+  return info;
+});
+
+export const loggerService = createLogger({
+  format: combine(
+    upperLevel(),
+    colorize({all: true}),
+    timestamp({format: "HH:mm:ss.sss"}),
+    printf(({level, message, timestamp}) => `[${timestamp}] ${level} (${process.pid}) : ${message}`),
+  ),
+  transports: [new transports.Console()]
 });
