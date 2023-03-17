4bfc336a47386d94f1be76a14c78ee751de23eee
Francesco Iesu <francesco.iesu@deepcrawl.com>
2023-03-17 09:54:10 +0000

    feat: public extension

--- azure-devops-extension/run-lumar-protect-build-task/src/_common/services/logger.service.ts
+++ azure-devops-extension/run-lumar-protect-build-task/src/_common/services/logger.service.ts
@@ -1,6 +1,6 @@
-import {createLogger, format, transports} from "winston";
+import { createLogger, format, transports } from "winston";
 
-const {colorize, combine, timestamp, printf} = format;
+const { colorize, combine, timestamp, printf } = format;
 const upperLevel = format(info => {
   info.level = info.level.toUpperCase();
   return info;
@@ -9,9 +9,9 @@ const upperLevel = format(info => {
 export const loggerService = createLogger({
   format: combine(
     upperLevel(),
-    colorize({all: true}),
-    timestamp({format: "HH:mm:ss.sss"}),
-    printf(({level, message, timestamp}) => `[${timestamp}] ${level} (${process.pid}) : ${message}`),
+    colorize({ all: true }),
+    timestamp({ format: "HH:mm:ss.sss" }),
+    printf(({ level, message, timestamp }) => `[${timestamp}] ${level} (${process.pid}) : ${message}`),
   ),
-  transports: [new transports.Console()]
+  transports: [new transports.Console()],
 });
